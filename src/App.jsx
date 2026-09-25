f6eeb639bfdbb51bbef3b49bda5e6c3bcb8115c4
Amit Goyal <amitpvt0150@gmail.com>
2026-09-25 18:35:34 +0530

    Configure for Github Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider, useAuth } from './context/AuthContext';
 import { ThemeProvider } from './context/ThemeContext';
 import { initLocalStorage, getData } from './utils/localStorage';
